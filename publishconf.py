f5d029ce7f9f4b6d97d66e9e26df8929d146264b
Adorilson Bezerra <adorilson@gmail.com>
2022-03-15 19:01:27 -0300

    Corrigi SITEURL em publishconf

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://2022.pythonbrasil.org.br/'
+SITEURL = 'http://meioambiente.cm.ifrn.edu.br/cm'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
