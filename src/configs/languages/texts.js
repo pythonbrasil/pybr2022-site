dd1272d569aa36922352a029406a36dab564a57e
Juliany Raiol <juliany.raiol@loggi.com>
2022-05-15 20:54:16 -0300

    add tradução para texto do plano de patrocínio

--- src/configs/languages/texts.js
+++ src/configs/languages/texts.js
@@ -1,20 +1,23 @@
+
 export const TEXT_PTBR = {
   LANDING_PAGE_TITLE: `17 a 23 de Outubro 2022`,
   LANDING_PAGE_INFO: `A submissão de palestras para o Python Brasil
     2022 pode ser feita pelo botão abaixo.`,
-    LANDING_PAGE_ACTION_BUTTON: `Submeta sua palestra aqui`
-  }
+    LANDING_PAGE_ACTION_BUTTON: `Submeta sua palestra aqui`,
+    LANGING_PAGE_BUTTON_PATROCINIO_TEXT: `Veja nosso plano de patrocínio`
+}
 
 export const TEXT_ES = {
   LANDING_PAGE_TITLE: `17 al 23 de octubre 2022`,
   LANDING_PAGE_INFO: `Envío de conferencias a Python Brasil
   2022 se puede hacer con el botón de abajo.`,
-  LANDING_PAGE_ACTION_BUTTON: `Submeta sua palestra aqui`
+  LANDING_PAGE_ACTION_BUTTON: `Envía tu charla aquí`
 }
 
 export const TEXT_EN = {
   LANDING_PAGE_TITLE: `From 17 to oct 23 2022`,
   LANDING_PAGE_INFO: `Lecture submission to Python Brazil
   2022 can be done by the button below.`,
-  LANDING_PAGE_ACTION_BUTTON: `Submeta sua palestra aqui`
+  LANDING_PAGE_ACTION_BUTTON: `Submit your talk here`,
+  LANGING_PAGE_BUTTON_PATROCINIO_TEXT: `See Sponsorship Options and Apply`
 }
