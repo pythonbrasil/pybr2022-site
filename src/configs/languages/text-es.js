40b368c035f9f052294cb72855c8b325b3e3cc55
Juliany Raiol <juliany.raiol@loggi.com>
2022-05-17 00:37:17 -0300

    separando arquivos de tradução

---
+++ src/configs/languages/text-es.js
@@ -0,0 +1,6 @@
+export const TEXT_ES = {
+    LANDING_PAGE_TITLE: `17 al 23 de octubre 2022`,
+    LANDING_PAGE_INFO: `Envío de conferencias a Python Brasil
+    2022 se puede hacer con el botón de abajo.`,
+    LANDING_PAGE_ACTION_BUTTON: `Envía tu charla aquí`
+}
