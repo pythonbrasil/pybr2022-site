bf67a65d87d031de655d53fde1c556cbf984d34e
richardson-souza <richrdson.allan.souza@gmail.com>
2022-05-19 12:41:17 -0400

    feature: add navbar component

---
+++ src/components/shared/NavBar/index.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import styled from "styled-components"
+import './style.scss';
+import NavbarLinks from "@components/shared/NavLinks";
+import Logo from "@components/shared/Logo";
+
+const Toggle = styled.div`
+  display: none;
+  height: 100%;
+  cursor: pointer;
+  padding: 0 10vw;
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`
+
+const Navbox = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    justify-content: flex-start;
+    padding-top: 10vh;
+    background-color: #4FB765;
+    transition: all 0.3s ease-in;
+    top: 8vh;
+    left: ${props => (props.open ? "-100%" : "0")};
+  }
+`
+
+const Hamburger = styled.div`
+  background-color: #fff;
+  width: 30px;
+  height: 3px;
+  transition: all .3s linear;
+  align-self: center;
+  position: relative;
+  transform: ${props => (props.open ? "rotate(-45deg)" : "inherit")};
+  ::before,
+  ::after {
+    width: 30px;
+    height: 3px;
+    background-color: #fff;
+    content: "";
+    position: absolute;
+    transition: all 0.3s linear;
+  }
+  ::before {
+    transform: ${props =>
+      props.open ? "rotate(-90deg) translate(-10px, 0px)" : "rotate(0deg)"};
+    top: -10px;
+  }
+  ::after {
+    opacity: ${props => (props.open ? "0" : "1")};
+    transform: ${props => (props.open ? "rotate(90deg) " : "rotate(0deg)")};
+    top: 10px;
+  }
+`
+
+const NavBar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+    return (
+        <nav class="navigation">
+            <Logo />
+            <Toggle
+                navbarOpen={navbarOpen}
+                onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+                {navbarOpen ? <Hamburger open /> : <Hamburger />}
+            </Toggle>
+            {navbarOpen ? (
+                <Navbox>
+                    <NavbarLinks />
+                </Navbox>
+            ) : (
+                <Navbox open>
+                    <NavbarLinks />
+                </Navbox>
+            )}
+        </nav>
+    )
+  }
+  
+  export default NavBar
