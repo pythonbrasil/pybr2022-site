fef4e8e3571ca34fbfbf45655e7b65351209f671
Felipe Alencar <alencar.dev@outlook.com>
2022-05-19 02:27:34 -0400

    Improve language switch and its sponsor plan file

---
+++ src/components/shared/Footer/index.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+import LogoImage from '@images/logo.svg';
+import YoutubeIcon from "@images/home/footer/youtube.svg";
+import TwitterIcon from "@images/home/footer/twitter.svg";
+import LinkedinIcon from "@images/home/footer/linkedin.svg";
+import FacebookIcon from "@images/home/footer/facebook.svg";
+import InstagramIcon from "@images/home/footer/instagram.svg";
+
+const Footer = ({ text }) => {
+  const socialPlatforms = [
+    { name: 'Instagram', icon: InstagramIcon, href: '' },
+    { name: 'Facebook', icon: FacebookIcon, href: '' },
+    { name: 'Linkedin', icon: LinkedinIcon, href: '' },
+    { name: 'Twitter', icon: TwitterIcon, href: '' },
+    { name: 'Youtube', icon: YoutubeIcon, href: '' },
+  ]
+
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-sm-3 logo-and-social">
+              <img
+                className="footerLogo"
+                src={LogoImage}
+                alt="Logo da Python Brasil 2022"
+              />
+
+              <ul className="social-platforms">
+                {socialPlatforms.map((platform) => (
+                  <li>
+                    <a href={platform.href}>
+                      <img
+                        className="icon"
+                        src={platform.icon}
+                        alt={platform.name}
+                      />
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="col-12 col-sm-3">
+              <p>{text.HOME.FOOTER.ABOUT_TEXT} </p>
+            </div>
+            <div className="col-12 col-sm-3">
+              <h4>Link rápidos</h4>
+
+              <ul className="links">
+                <li>
+                  <a href="/">Python Brasil 2022</a>
+                </li>
+                <li>
+                  <a href="/palestras">Submissão de Palestras</a>
+                </li>
+                <li>
+                  <a href="/schedule">Programação</a>
+                </li>
+                <li>
+                  <a href="/faq">FAQ</a>
+                </li>
+                <li>
+                  <a href="/duty">Código de Conduta</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-12 col-sm-3">
+              <h4>Endereço do Evento</h4>
+
+              <p>Centro de Convenções do Amazonas Vasco Vasques</p>
+              <br />
+
+              <p>Av. Constantino Nery, 5001 - Flores, Manaus - AM, 69058-795</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="privacy-policy">
+        <div className="container">
+          <span>Política de privacidade</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- src/configs/languages/text-en.js
+++ src/configs/languages/text-en.js
@@ -1,8 +1,20 @@
 export const TEXT_EN = {
-  LANDING_PAGE_TITLE: `From 17 to oct 23 2022`,
-  LANDING_PAGE_INFO: `Lecture submission to Python Brazil
-  2022 can be done by the button below.`,
-  LANDING_PAGE_ACTION_BUTTON: `Submit your talk here`,
-  LANGING_PAGE_BUTTON_PATROCINIO_TEXT: `See Sponsorship Options and Apply`,
-  LANDING_TEXT_RODAPE: `PythonBrasil is a non-profit volunteer-driven conference for promoting the open-source Python programming language. It is supported by the Brazilian Python Association (APyB) and by the Python Software Foundation (PSF).`
+  HOME: {
+    LANDING: {
+      TITLE: 'From 17 to oct 23 2022',
+      INFO: 'Lecture submission to Python Brazil 2022 can be done by the button below.',
+      BUTTON: 'Submit your talk here'
+    },
+    SPONSORING: {
+      BUTTON: 'See Sponsorship Options and Apply'
+    },
+    FOOTER: {
+      ABOUT_TEXT: 'PythonBrasil is a non-profit volunteer-driven conference for promoting the open-source Python programming language. It is supported by the Brazilian Python Association (APyB) and by the Python Software Foundation (PSF).'
+    }
+  },
+  CDC: {
+    LANDING: {
+      TITLE: `Código de Conduta`
+    }
+  }
 }
