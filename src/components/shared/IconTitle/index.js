bd9c84283c613007da82abab4da2891346306884
= <dco3@cin.ufpe.br>
2022-05-22 23:35:41 -0300

    add responsiveness to iconTitle component

--- src/components/shared/IconTitle/index.js
+++ src/components/shared/IconTitle/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
-import './style.scss';
+import "./style.scss";
 
 const IconTitle = ({ title, icon }) => (
   <div className="icon-title">
-    <img src={icon} alt={title} />
+    <h4 className="icon-title__mobile">#PYTHONBRASIL2022</h4>
     <h2>{title}</h2>
-    <h4>#PYTHONBRASIL2022</h4>
+    <h4 className="icon-title__desktop">#PYTHONBRASIL2022</h4>
+    <img src={icon} alt={title} />
   </div>
-)
+);
 
-export default IconTitle
+export default IconTitle;
