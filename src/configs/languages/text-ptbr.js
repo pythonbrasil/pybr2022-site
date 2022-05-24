5d8345d06475151d788fdebf422c0f816552dbe7
= <dco3@cin.ufpe.br>
2022-05-24 15:29:17 -0300

    fix responsividade sobre section

--- src/configs/languages/text-ptbr.js
+++ src/configs/languages/text-ptbr.js
@@ -3,18 +3,19 @@ export const TEXT_PTBR = {
     LANDING: {
       TITLE: `17 a 23 de Outubro 2022`,
       INFO: `A submissão de palestras para o Python Brasil 2022 pode ser feita pelo botão abaixo.`,
-      BUTTON: `Submeta sua palestra aqui`
+      BUTTON: `Submeta sua palestra aqui`,
     },
     SPONSORING: {
-      BUTTON: `Veja nosso plano de patrocínio`
+      TITLE: "Saiba como patrocinar o evento",
+      BUTTON: `Veja nosso plano de patrocínio`,
     },
     FOOTER: {
-      ABOUT_TEXT: `PythonBrasil é uma conferência sem fins lucrativos dirigida por voluntários para promover a linguagem de programação Python de código aberto. É apoiado pela Associação Python Brasil (APyB) e pela Python Software Foundation (PSF).`
-    }
+      ABOUT_TEXT: `PythonBrasil é uma conferência sem fins lucrativos dirigida por voluntários para promover a linguagem de programação Python de código aberto. É apoiado pela Associação Python Brasil (APyB) e pela Python Software Foundation (PSF).`,
+    },
   },
   CDC: {
     LANDING: {
-      TITLE: `Código de Conduta`
-    }
-  }
-}
+      TITLE: `Código de Conduta`,
+    },
+  },
+};
