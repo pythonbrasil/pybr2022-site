37cc204192fac9a11dcf351aa38fed648dd2d0d3
Alynne Ferreira <alynneferreiras@gmail.com>
2022-05-24 23:39:01 -0300

    facilita a visualização das tags

--- src/components/pages/GuiaPage/index.js
+++ src/components/pages/GuiaPage/index.js
@@ -31,16 +31,22 @@ const GuiaPage = ({ text }) => {
           <div className="row">
             <div className="col-12 col-sm-9">
               <p>
-              Como forma de orientar e garantir a segurança dos visitantes na Python Br 22, é de grande importância se atentar as questões sanitárias e de cuidados pessoais durante a estadia na cidade.
+                Como forma de orientar e garantir a segurança dos visitantes na Python Br 22, é de grande importância se atentar as questões sanitárias e de cuidados pessoais durante a estadia na cidade.
               </p>
 
-              <p>O Centro de Convenções Vasco Vasques, local escolhido pela organização do evento, possui grandes corredores e salões onde serão realizadas as atividades principais do evento, além de áreas externas acessíveis que terão seu uso incentivado.
-</p>
+              <p>
+                O Centro de Convenções Vasco Vasques, local escolhido pela organização do evento, possui grandes corredores e salões onde serão realizadas as atividades principais do evento, além de áreas externas acessíveis que terão seu uso incentivado.
+              </p>
               <p>Com obrigatoriedade de comprovante de vacinações, estímulo do uso das àreas externas, regras de distanciamento, higienização dos espaços, cuidados com a boa ventilação dos ambientes, uma equipe dedicada ao controle de fluxo de pessoas e kits de higiene acessíveis, colocaremos os cuidados com a saúde das participantes como prioridade.</p>
               
               <br />
-              <p><h1>Covid-19</h1></p>
-              <p>Além dos cuidados adotados pela organização do evento, a colaboração dos participantes é vital para conter a disseminação do virus, a prevenção é uma das ferramentas mais eficientes para manter a segurança durante o evento:</p>
+              <p>
+                <h1>Covid-19</h1>
+              </p>
+              
+              <p>
+                Além dos cuidados adotados pela organização do evento, a colaboração dos participantes é vital para conter a disseminação do virus, a prevenção é uma das ferramentas mais eficientes para manter a segurança durante o evento:
+              </p>
 
               <p>1. Use máscaras de proteção facial corretamente, cobrindo nariz e boca;</p>
               
@@ -53,7 +59,9 @@ const GuiaPage = ({ text }) => {
               <p>5. Isole-se ao apresentar sintomas como: Tosse, febre e dor de garganta;</p>
               
               <br />
-              <p><h1>Vacinas:</h1></p>
+              <p>
+                <h1>Vacinas:</h1>
+              </p>
               
               <ul>
                 <li>
@@ -69,15 +77,17 @@ const GuiaPage = ({ text }) => {
 	      </p>              
               
               <br />
-              <p><h1>Referências:</h1></p>
+              <p>
+                <h1>Referências:</h1>
+              </p>
               
               <p>
                 SEMSA Manaus. GUIA DE SAÚDE PARA O TURISTA, 2021. Disponível em: <a href="https://semsa.manaus.am.gov.br/servico_cidadao/guia-de-saude-para-o-turista">https://semsa.manaus.am.gov.br/servico_cidadao/guia-de-saude-para-o-turista/</a> Acesso em: 02 de abril de 2022.
-             </p>
+              </p>
 
-             <p>
+              <p>
                Gov.br. Vacinação. Disponível em: <a href="https://www.gov.br/saude/pt-br/vacinacao/">https://www.gov.br/saude/pt-br/vacinacao/</a> Acesso em: Acesso em: 02 de abril de 2022.
-             </p>
+              </p>
 
               <span className="last-update">
                 Última atualização: 24 de Maio de 2022
