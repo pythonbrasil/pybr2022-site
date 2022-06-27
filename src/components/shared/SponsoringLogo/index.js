ea9345fe455d47f5bd923bc24bf7e46fa9f2b3ae
Richardson Souza <richardson.allan.souza@gmail.com>
2022-06-27 16:15:09 -0400

    feature: add Sponsoring component and style

---
+++ src/components/shared/SponsoringLogo/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./style.scss";
+import TitleChip from "@components/shared/TitleChip";
+import JusBrasilLogo from "@images/sponsoring/jusbrasil.svg";
+import Stream from "@images//sponsoring/stream.svg";
+
+const SponsoringLogo = ({ text }) => {
+  return (
+    <>
+      <section id="section-sponsoring-logos">
+        <div className="container">
+          <div className="row section-sponsoring-logos__title">
+            <TitleChip>{text.HOME.SPONSORINGLOGO.TITLE}</TitleChip>
+            <h2>diamante</h2>
+            <h2>platina</h2>
+            <h2>ouro</h2>
+            <h2>prata</h2>
+            <h2>cobre</h2>
+            <a href="https://getstream.io/">
+              <img src={Stream} alt="Stream" width="400px" height="100px"/>
+            </a>
+            <h2>bronze</h2>
+            <a href="https://www.jusbrasil.com.br/">
+              <img src={JusBrasilLogo} alt="JusBrasil" width="400px" height="100px"/>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SponsoringLogo;
