d93c4fd4bd125de284f41a039505449f61f73b66
Richardson Souza <richardson.allan.souza@gmail.com>
2022-07-14 18:55:49 -0400

    feature: include google and packt logo

--- src/components/shared/SponsoringLogo/index.js
+++ src/components/shared/SponsoringLogo/index.js
@@ -3,7 +3,9 @@ import React from "react";
 import "./style.scss";
 import TitleChip from "@components/shared/TitleChip";
 import JusBrasilLogo from "@images/sponsoring/jusbrasil.svg";
-import Stream from "@images//sponsoring/stream.svg";
+import Stream from "@images/sponsoring/stream.svg";
+import Google from "@images/sponsoring/google.png";
+import SupportLogo from "@components/shared/SupportLogo";
 
 const SponsoringLogo = ({ text }) => {
   return (
@@ -12,6 +14,10 @@ const SponsoringLogo = ({ text }) => {
         <div className="container">
           <div className="row section-sponsoring-logos__title">
             <TitleChip>{text.HOME.SPONSORINGLOGO.TITLE}</TitleChip>
+            <h2>prata</h2>
+            <a href="https://opensource.google/">
+              <img src={Google} alt="Google believes that open source is good for everyone. By being open and freely available, it enables and encourages collaboration and the development of technology."/>
+            </a>
             <h2>bronze</h2>
             <a href="https://www.jusbrasil.com.br/">
               <img src={JusBrasilLogo} alt="JusBrasil"/>
@@ -22,6 +28,7 @@ const SponsoringLogo = ({ text }) => {
             </a>
           </div>
         </div>
+        <SupportLogo/>
       </section>
     </>
   );
