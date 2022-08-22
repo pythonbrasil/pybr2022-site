499fb790b35cd63a661d70f4bfac4694bbbcfe63
cecivieira <cecivieira@gmail.com>
2022-08-21 22:59:56 -0300

    adiciona tipo titulo no texto

--- src/components/shared/DutyContent/index.js
+++ src/components/shared/DutyContent/index.js
@@ -8,16 +8,32 @@ const DutyContent = ({ text }) => {
       {text.map((text) => {
         const isArray = text.content[0].length > 1;
         const contentKey = (isArray && text.content[0]) || text.content;
-        const content =
-          text.type === "paragraph" ? (
-            <p key={contentKey}>{text.content}</p>
-          ) : (
-            <ul key={`list-${contentKey}`}>
+        // const content =
+        //   text.type === "paragraph" ? (
+        //     <p key={contentKey}>{text.content}</p>
+        //   ) : (
+        //     <ul key={`list-${contentKey}`}>
+        //       {text.content.map((item) => (
+        //         <li key={`item-${item}`}>{item} </li>
+        //       ))}
+        //     </ul>
+        //   );
+        var type_content =  text.type
+        var content = ""
+        if (type_content === "paragraph") {
+          content= (<p key={contentKey}>{text.content}</p>)
+        } 
+        else if (type_content === "title") {
+          content= (<h3 key={contentKey}>{text.content}</h3>)
+        }
+        else {
+          content =
+          (<ul key={`list-${contentKey}`}>
               {text.content.map((item) => (
                 <li key={`item-${item}`}>{item} </li>
               ))}
-            </ul>
-          );
+            </ul>)
+        }
         return content;
       })}
     </>
