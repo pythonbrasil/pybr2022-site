a0651df88f6e221e6de6dffcff30aacd5d0c2cc7
Felipe Oliveira Colen <felipecolen@users.noreply.github.com>
2022-10-11 10:13:51 -0400

    removida logo da packt (#122)

--- src/components/shared/SupportLogo/index.js
+++ src/components/shared/SupportLogo/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import "./style.scss";
 import TitleChip from "@components/shared/TitleChip";
-import Packt from "@images/supporting/packt.svg";
 import Ocean from "@images/supporting/ocean.png";
 import Psf from "@images/supporting/psf.svg";
 
@@ -19,13 +18,8 @@ const SupportLogo = ({ text }) => {
               <img src={Psf} alt="Python Software Foundation"/>
             </a>
 
-            <div></div>
-
             <a href="https://www.oceanbrasil.com/">
               <img src={Ocean} alt="O Ocean é o programa de capacitação e inovação tecnológica da Samsung, em parceria com universidades brasileiras de referência."/>
-            </a>
-            <a href="https://www.packt.com/">
-              <img src={Packt} alt="Packt"/>
             </a>
 
           </div>
