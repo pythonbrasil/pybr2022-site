167a5f4a97c48b40018552665d29329572cd9a08
Felipe Oliveira Colen <felipecolen@gmail.com>
2022-10-11 10:54:16 -0400

    feat: adicionada logo do sebrae

--- src/components/shared/SupportLogo/index.js
+++ src/components/shared/SupportLogo/index.js
@@ -4,6 +4,7 @@ import "./style.scss";
 import TitleChip from "@components/shared/TitleChip";
 import Ocean from "@images/supporting/ocean.png";
 import Psf from "@images/supporting/psf.svg";
+import SebraeLab from "@images/supporting/sebraelab.png";
 
 
 const SupportLogo = ({ text }) => {
@@ -18,8 +19,14 @@ const SupportLogo = ({ text }) => {
               <img src={Psf} alt="Python Software Foundation"/>
             </a>
 
+            <div></div>
+
             <a href="https://www.oceanbrasil.com/">
               <img src={Ocean} alt="O Ocean é o programa de capacitação e inovação tecnológica da Samsung, em parceria com universidades brasileiras de referência."/>
+            </a>
+
+            <a href="https://bit.ly/sebraelabam">
+              <img src={SebraeLab} alt="O Sebraelab faz parte do SEBRAE. É um espaço de estímulo à criatividade, à inovação, ao consumo de informações, à geração de novos conhecimentos, ao aprendizado contínuo e às múltiplas conexões nos negócios."/>
             </a>
 
           </div>
